cb243adab93db914dce04132504b14fd9bd0d971
Arief Wijaya <hoaaah.arief@gmail.com>
2018-04-25 23:23:59 +0700

    Form View
    With Dropdown and JUI Datepicker

--- modules/administrator/views/blog/_form.php
+++ modules/administrator/views/blog/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Blog */
@@ -18,13 +20,17 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    <?= $form->field($model, 'created_at')->widget(DatePicker::className(), ['options' => ['class' => 'form form-control']]) ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?= $form->field($model, 'updated_at')->widget(DatePicker::className(), ['options' => ['class' => 'form form-control']]) ?>
 
-    <?= $form->field($model, 'created_by')->textInput() ?>
+    <?= $form->field($model, 'created_by')->dropdownList(ArrayHelper::map($users, 'id', 'username'),
+        ['prompt'=>'Select User']
+    ); ?>
 
-    <?= $form->field($model, 'updated_by')->textInput() ?>
+    <?= $form->field($model, 'updated_by')->dropdownList(ArrayHelper::map($users, 'id', 'username'),
+        ['prompt'=>'Select User']
+    ); ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
