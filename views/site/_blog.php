a0dfd5605bff716840106606a8b0de2047a3f752
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2018-05-23 22:54:16 +0700

    .

--- views/site/_blog.php
+++ views/site/_blog.php
@@ -8,6 +8,11 @@ use yii\helpers\StringHelper;
 <div class="post">
     <h2><?= Html::a(Html::encode($model->title), ['/blog/'.$model->slug]) ?></h2>
 
+    <div class="btn-group" role="group" aria-label="...">
+        <?php // Html::a("Username", false, ['class' => 'btn btn-default']) ?>
+        <?= Html::a('<i class="glyphicon glyphicon-calendar" ></i> '.Yii::$app->formatter->asDate($model->created_at), false, ['class' => 'btn btn-xs btn-default']) ?>
+    </div>
+
     <?= StringHelper::truncateWords(HtmlPurifier::process($model->body), 30, Html::a("Read More &raquo;", ['/blog/'.$model->slug], [
         'class' => 'btn btn-default'
     ]), true) ?>
