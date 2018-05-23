f4c6e5072782faad4a50d25ee2a371c8c0d138e5
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2018-05-23 22:27:24 +0700

    Display date in view

--- views/blog/view.php
+++ views/blog/view.php
@@ -7,7 +7,24 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="post">
-    <h2><?= Html::encode($model->title) ?></h2>
+    <div class="row">
+        <div class="col-md-10">
+            <h2><?= Html::encode($model->title) ?></h2>
+        </div>
+    </div>
 
-    <?= HtmlPurifier::process($model->body) ?>    
+    <div class="row">
+        <div class="col-md-10">    
+            <div class="btn-group" role="group" aria-label="...">
+                <?php // Html::a("Username", false, ['class' => 'btn btn-default']) ?>
+                <?= Html::a('<i class="glyphicon glyphicon-calendar" ></i> '.Yii::$app->formatter->asDate($model->created_at), false, ['class' => 'btn btn-default']) ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10">    
+            <?= HtmlPurifier::process($model->body) ?>    
+        </div>
+    </div>
 </div>
