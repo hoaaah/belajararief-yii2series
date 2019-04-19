360b47efcf3a9c1f7ad1b7fabca0dcd531224e56
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2019-04-19 15:23:28 +0700

    Change pull- to float-

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -73,9 +73,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="float-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
