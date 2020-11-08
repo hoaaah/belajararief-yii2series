7a7fad426a5a10afa6b5d385ba446d183a0072cc
Arief Wijaya <hoaaah.arief@gmail.com>
2020-11-08 16:29:25 +0700

    Create gii template

---
+++ templates/modalcrud/views/_form.php
@@ -0,0 +1,70 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+/* @var $model \yii\db\ActiveRecord */
+$model = new $generator->modelClass();
+$safeAttributes = $model->safeAttributes();
+if (empty($safeAttributes)) {
+    $safeAttributes = $model->attributes();
+}
+
+echo "<?php\n";
+?>
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-form">
+
+    <?= "<?php " ?>$form = ActiveForm::begin(['id' => $model->formName()]); ?>
+
+<?php foreach ($generator->getColumnNames() as $attribute) {
+    if (in_array($attribute, $safeAttributes)) {
+        echo "    <?= " . $generator->generateActiveField($attribute) . " ?>\n\n";
+    }
+} ?>
+    <div class="form-group">
+        <?= "<?= " ?>Html::submitButton($model->isNewRecord ? <?= $generator->generateString('Create') ?> : <?= $generator->generateString('Update') ?>, ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?= "<?php " ?>ActiveForm::end(); ?>
+
+</div>
+<?= "<?php " ?>
+$script = <<<JS
+$('form#{$model->formName()}').on('beforeSubmit',function(e)
+{
+    var \$form = $(this);
+    $.post(
+        \$form.attr("action"), //serialize Yii2 form 
+        \$form.serialize()
+    )
+        .done(function(result){
+            if(result == 1)
+            {
+                $("#myModal").modal('hide'); //hide modal after submit
+                //$(\$form).trigger("reset"); //reset form to reuse it to input
+                $.pjax.reload({container:'#<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-pjax'});
+            }else
+            {
+                $("#message").html(result);
+            }
+        }).fail(function(){
+            console.log("server error");
+        });
+    return false;
+});
+
+JS;
+$this->registerJs($script);
+?>
---
+++ templates/modalcrud/views/index.php
@@ -0,0 +1,161 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+$nameAttribute = $generator->getNameAttribute();
+
+echo "<?php\n";
+?>
+
+use yii\helpers\Html;
+use <?= $generator->indexWidgetType === 'grid' ? "kartik\\grid\\GridView" : "yii\\widgets\\ListView" ?>;
+<?= $generator->enablePjax ? 'use yii\widgets\Pjax;' : '' ?>
+use yii\bootstrap4\Modal;
+
+/* @var $this yii\web\View */
+<?= !empty($generator->searchModelClass) ? "/* @var \$searchModel " . ltrim($generator->searchModelClass, '\\') . " */\n" : '' ?>
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>;
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
+
+    <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
+<?php if(!empty($generator->searchModelClass)): ?>
+<?= "    <?php " . ($generator->indexWidgetType === 'grid' ? "// " : "") ?>echo $this->render('_search', ['model' => $searchModel]); ?>
+<?php endif; ?>
+
+    <p>
+        <?= "<?= " ?>Html::a(<?= $generator->generateString('Tambah ' . Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>, ['create'], [
+                                                    'class' => 'btn btn-xs btn-success',
+                                                    'data-toggle'=>"modal",
+                                                    'data-target'=>"#myModal",
+                                                    'data-title'=> <?= $generator->generateString("Tambah " . Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>,
+                                                    ]) ?>
+    </p>
+<?php if ($generator->indexWidgetType === 'grid'): ?>
+    <?= "<?= " ?>GridView::widget([
+        'id' => '<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>',    
+        'dataProvider' => $dataProvider,
+        'export' => true, 
+        'responsive'=>true,
+        'hover'=>true,     
+        'resizableColumns'=>true,
+        'panel'=>['type'=>'primary', 'heading'=>$this->title],
+        'responsiveWrap' => false,        
+        'toolbar' => [
+            [
+                'content' => '{export}{toggleData}' // $this->render('_search', ['model' => $searchModel]),
+            ],
+        ],
+        // set export properties
+        'export' => [
+            'fontAwesome' => true
+        ],  
+        'pager' => [
+            'firstPageLabel' => 'Awal',
+            'lastPageLabel'  => 'Akhir'
+        ],
+        'pjax'=>true,
+        'pjaxSettings'=>[
+            'options' => ['id' => '<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-pjax', 'timeout' => 5000],
+        ],        
+        <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n        'columns' => [\n" : "'columns' => [\n"; ?>
+            ['class' => 'yii\grid\SerialColumn'],
+
+<?php
+$count = 0;
+if (($tableSchema = $generator->getTableSchema()) === false) {
+    foreach ($generator->getColumnNames() as $name) {
+        if (++$count < 6) {
+            echo "            '" . $name . "',\n";
+        } else {
+            echo "            // '" . $name . "',\n";
+        }
+    }
+} else {
+    foreach ($tableSchema->columns as $column) {
+        $format = $generator->generateColumnFormat($column);
+        if (++$count < 6) {
+            echo "            '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+        } else {
+            echo "            // '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+        }
+    }
+}
+?>
+
+            [
+                'class' => 'kartik\grid\ActionColumn',
+                'template' => '{view} {update} {delete}',
+                'noWrap' => true,
+                'vAlign'=>'top',
+                'buttons' => [
+                        'update' => function ($url, $model) {
+                          return Html::a('<span class="fas fa-edit"></span>', $url,
+                              [  
+                                 'title' => Yii::t('yii', 'ubah'),
+                                 'data-toggle'=>"modal",
+                                 'data-target'=>"#myModal",
+                                 'data-title'=> "Ubah",                                 
+                                 // 'data-confirm' => "Yakin menghapus ini?",
+                                 // 'data-method' => 'POST',
+                                 // 'data-pjax' => 1
+                              ]);
+                        },
+                        'view' => function ($url, $model) {
+                          return Html::a('<span class="fas fa-eye"></span>', $url,
+                              [  
+                                 'title' => Yii::t('yii', 'lihat'),
+                                 'data-toggle'=>"modal",
+                                 'data-target'=>"#myModal",
+                                 'data-title'=> "Lihat",
+                              ]);
+                        },                        
+                ]
+            ],
+        ],
+    ]); ?>
+<?php else: ?>
+    <?= "<?= " ?>ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model-><?= $nameAttribute ?>), ['view', <?= $urlParams ?>]);
+        },
+    ]) ?>
+<?php endif; ?>
+</div>
+<?= "<?php " ?>
+Modal::begin([
+    'id' => 'myModal',
+    'title' => 'Lihat lebih...',
+    'options' => [
+        'tabindex' => false // important for Select2 to work properly
+    ], 
+]);
+ 
+echo '...';
+ 
+Modal::end();
+$this->registerJs("
+    $('#myModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget)
+        var modal = $(this)
+        var title = button.data('title') 
+        var href = button.attr('href') 
+        modal.find('.modal-title').html(title)
+        modal.find('.modal-body').html('<i class=\"fa fa-spinner fa-spin\"></i>')
+        $.post(href)
+            .done(function( data ) {
+                modal.find('.modal-body').html(data)
+            });
+        })
+");
+?>
